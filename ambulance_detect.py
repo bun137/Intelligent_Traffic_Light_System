bde6b70f0527779e8261a059987d86781e738453
bwaklog <aditya.mh@outlook.com>
2023-10-14 02:24:14 +0530

    using fouriers transformation for filtering signal

--- ambulance_detect.py
+++ ambulance_detect.py
@@ -1,14 +1,18 @@
+from scipy.fft import fft, fftfreq
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import numpy as np
 
-rate, data = wavfile.read('amb.wav')
-length = data.shape[0] / rate
-# print(data.shape[0])
-# time = np.linspace(0., length, data.shape[0])
+sample_rate, data = wavfile.read('amb.wav')
+length = data.shape[0] / sample_rate
+print(data.shape[0])
+time = np.linspace(0., length, data.shape[0])
 # plt.plot(time, data, label="Audio")
-print(len(set(data)))
 # plt.legend()
 # plt.xlabel("Time [sS]")
 # plt.ylabel("Amplitude")
 # plt.show()
+
+# filtering signal
+N = sample_rate * time
+print(N)
