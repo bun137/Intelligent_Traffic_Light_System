7f895a648f3926ee6e0ef2558e388f1576a37d68
bwaklog <aditya.mh@outlook.com>
2023-10-14 05:47:54 +0530

    now able to verfiy (70% sure) that there is a siren in the background - limitations : Yet to verify that there is an osicllating frequency

--- ambulance_detect.py
+++ ambulance_detect.py
@@ -3,30 +3,49 @@ from scipy.io import wavfile
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 import numpy as np
+from pydub import AudioSegment
 
-sample_rate, audio_data = wavfile.read('amb.wav')
-length = audio_data.shape[0] / sample_rate
-audio_data = np.abs(audio_data)
-# print(audio_data.shape[0])
-time = np.linspace(0., length, audio_data.shape[0])
-# plt.plot(time, data, label="Audio")
-# plt.legend()
-# plt.xlabel("Time [sS]")
-# plt.ylabel("Amplitude")
-# plt.show()
+# --------------------------------------------
+# converting mp3 to wav format
+# mp3_file = "citytraffic.mp3"
+# wav_file = "citytraffic.wav"
+# audio = AudioSegment.from_mp3(mp3_file)
+# audio.export(wav_file, format="wav")
+# --------------------------------------------
 
-# filtering signal
-max_frequency = sample_rate / 2
-print(f"max freq : {max_frequency}")
+# file_str -> filename / path to .wav file
 
 
-# peaks in audio file
-if len(audio_data.shape) > 1:
-    audio_data = audio_data.mean(axis=1)
-peaks, _ = find_peaks(audio_data, height=0)
+def audio_analysis(file_str: str):
+    sample_rate, audio_data = wavfile.read(file_str)
+    length = audio_data.shape[0] / sample_rate
+    audio_data = np.abs(audio_data)
+    # print(audio_data.shape[0])
+    time = np.linspace(0., length, audio_data.shape[0])
+    # plt.plot(time, audio_data, label="Audio")
+    # plt.legend()
+    # plt.xlabel("Time [sS]")
+    # plt.ylabel("Amplitude")
+    # plt.show()
 
-# average of peaks
-peak_values = audio_data[peaks]
-print(peak_values[len(peak_values)//2:])
-average_peak_values = np.mean(peak_values)
-print(f"average peak values : {average_peak_values}")
+    # filtering signal
+    max_frequency = sample_rate / 2
+    # print(f"max freq : {max_frequency}")
+
+    # peaks in audio file
+    if len(audio_data.shape) > 1:
+        audio_data = audio_data.mean(axis=1)
+    peaks, _ = find_peaks(audio_data, height=0)
+
+    # average of peaks
+    mean_wave_value = np.mean(audio_data)
+    print(f"mean wave vallue : {mean_wave_value}")
+    peak_values = audio_data[peaks]
+    print(peak_values[len(peak_values)//2:])
+    mean_peak_values = np.mean(peak_values)
+    print(f"average peak values : {mean_peak_values}")
+
+
+audio_analysis('citytraffic.wav')
+print('\n\n')
+audio_analysis('amb.wav')
